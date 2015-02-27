a284c08be083b69787166fe59112f0740259debe
Emanuele DelBono <emanuele@codiceplastico.com>
2015-02-27 16:39:58 +0100

    step 8 - flux

--- app/app.js
+++ app/app.js
@@ -1,14 +1,20 @@
 var Container = React.createClass({
   getInitialState: function(){
     return {
-      todos: ['learn javascript', 'write some code'],
+      todos: TodoStore.getAll(),
       todo: ''
     }
   },
+  componentDidMount: function(){
+    TodoStore.addSubscriber(this.onChange);
+  },
+  onChange: function(){
+    var todos = TodoStore.getAll();
+    this.setState({todos: todos});
+  },
   handleClick:  function(){
-    this.state.todos.push(this.state.todo);
-    this.setState({todos: this.state.todos});
     this.setState({todo: ''});
+    TodoActions.create(this.state.todo);
   },
   handleChange: function(event) {
     this.setState({todo: event.target.value});
@@ -32,6 +38,52 @@ var Container = React.createClass({
 var TodoItem = React.createClass({
   render: function(){
     return (<li>{this.props.text}</li>);
+  }  
+});
+
+// FLUX
+var AppDispatcher = new Flux.Dispatcher();
+var TodoConstants = {
+  TODO_CREATE: 'TODO_CREATE'
+};
+
+// ACTIONS
+var TodoActions = {
+  create: function(text) {
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_CREATE,
+      text: text
+    });
+  }
+};
+
+// STORE
+var TodoStore = {
+  _items: ['learn javascript', 'write some code'],
+  _subscribers: [],
+  create: function(text){
+    this._items.push(text);
+    this.emitChange();
+  },
+  getAll: function(){
+    return this._items;
+  },
+  emitChange: function(){
+    this._subscribers.forEach(function(s){
+      s();
+    });
+  },
+  addSubscriber: function(callback){
+    this._subscribers.push(callback);
+  }
+};
+
+// DISPATCHER
+AppDispatcher.register(function(action) {
+  switch(action.actionType) {
+    case TodoConstants.TODO_CREATE:
+      TodoStore.create(action.text);
+      break;
   }  
 });
 
